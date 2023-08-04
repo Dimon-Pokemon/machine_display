6f15f8993cd5b349cd2f7297ea30407d5b342de7
Dimon-Pokemon <purumamuma@gmail.com>
2023-08-04 15:54:28 +0400

    Удалил лишний атрибут (радиус r) класса Point

--- point.py
+++ point.py
@@ -2,12 +2,11 @@ class Point:
 
     x = 0
     y = 0
-    r = 0
     id = None
     state = None
     color = None
 
-    def __init__(self, id, state, color, x=0, y=0, r=6):
+    def __init__(self, id, state, color, x=0, y=0):
         self.x = x
         self.y = y
         self.id = id
